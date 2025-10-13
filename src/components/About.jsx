c6ca7731740cb8905bad4820464c453e1f97058d
Ebikemeese <ebikemeese257@gmail.com>
2025-10-13 06:33:50 +0100

    pre-deploy

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -44,14 +44,14 @@ const About = () => {
                 <div className="about-subtext">
                     <p>The Game of Games begins-your life, now an epic MMORPG</p>
                 
-                    <p>Zentry units every player from countless games and platforms</p>
+                    <p className="text-gray-500">Zentry units every player from countless games and platforms</p>
                 </div>
             </div>
 
             <div className="h-dvh w-screen" id="clip">
                 <div className="mask-clip-path about-image">
                     <img 
-                        src="img/about.webp" 
+                        src="/Award-winnig/img/about.webp" 
                         alt="Background" 
                         className="absolute left-0 top-0 size-full object-cover" 
                     />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -67,7 +67,7 @@ const Navbar = () => {
                 <nav className="flex size-full items-center justify-between p-4">
                     <div className="flex items-center gap-7">
                         <img
-                            src="/img/logo.png"
+                            src="/Award-winnig/img/logo.png"
                             alt="logo"
                             className="w-10"
                         />
